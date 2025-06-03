ebf7f0deca943e0411c668b4600612494b7511c4
carinezoe <zoecleo.dreessen@gmail.com>
2025-06-03 21:55:21 +0200

    Added show/hide complete info of creatures/hazards hover

--- resources/views/builder/layout.blade.php
+++ resources/views/builder/layout.blade.php
@@ -29,7 +29,7 @@
                 <!-- list creatures -->
                 <div class="h-84 overflow-y-auto scrollbar-hide divide-y-1 divide-y divide-tertiary">
                     @foreach($creatures as $creature)
-                        <div class="p-2">
+                        <div class="p-2" id="{{$creature->id}}" onmouseover="showCreatureInfo(id)" onmouseout="hideCreatureInfo(id)">
                             <div class="flex flex-row justify-between">
                                 <div class="text-lg">{{$creature->name}}</div>
                                 <div class="flex flex-row gap-2">
@@ -52,7 +52,7 @@
                 <!-- list hazard -->
                 <div class="h-36 overflow-y-auto scrollbar-hide  divide-y-1 divide-y divide-tertiary">
                     @foreach($hazards as $hazard)             
-                        <div class="flex flex-row justify-between p-2">
+                        <div class="flex flex-row justify-between p-2" id="{{$hazard->id}}" onmouseover="showHazardInfo(id)" onmouseout="hideHazardInfo(id)">
                             <div>{{$hazard->name}}</div>
                             <div class="bg-tertiary p-1 rounded-lg">{{$hazard->type->name}}</div>
                         </div>
@@ -72,10 +72,77 @@
             </div>
         </div>
         <!-- hover popup -->
-        <div class="bg-secondary w-2/3 m-2 p-2 border border-accent rounded-lg hidden" id="popup">test</div>
+        <div class="bg-secondary w-2/3 m-2 p-2 border border-accent rounded-lg hidden" id="popup"></div>
         <!-- selected content -->
-        <div class="w-2/3" id="content">
+        <div class="w-2/3 m-2" id="content">
             @yield('content')
         </div>
     </div>
 </div>
+
+<script>
+// Get elements
+const hazards = @json($hazards);
+const creatures = @json($creatures);
+const popup = document.getElementById('popup');
+const content = document.getElementById('content');
+
+// Show the complete hazard info
+function showHazardInfo(id) {
+    let selectedHazard = hazards.find(hazard => hazard.id == id);
+    popup.classList.remove('hidden');
+    content.classList.add('hidden');
+    
+    const traitsHtml = selectedHazard.pathfindertraits
+        .map(trait => `<div>${trait.name}</div>`)
+        .join('');
+
+    popup.innerHTML = `
+        <div>${selectedHazard.name}</div>
+        <div>${selectedHazard.complexity}</div>
+        <div>${selectedHazard.type.name}</div>
+        <div>${selectedHazard.rarity.name}</div>
+        <div class="flex flex-row">${traitsHtml}</div>
+        <div>${selectedHazard.source}</div>
+    `;
+}
+
+// Hide the complete hazard info
+function hideHazardInfo(id) {
+    popup.classList.add('hidden');
+    content.classList.remove('hidden');
+}
+
+// Show the complete creature info
+function showCreatureInfo(id) {
+    let selectedCreature = creatures.find(creature => creature.id == id);
+    popup.classList.remove('hidden');
+    content.classList.add('hidden');
+    
+    const traitsHtml = selectedCreature.pathfindertraits
+        .map(trait => `<div>${trait.name}</div>`)
+        .join('');
+
+    popup.innerHTML = `
+        <div>${selectedCreature.name}</div>
+        <div>${selectedCreature.size.name}</div>
+        <div>${selectedCreature.level}</div>
+        <div>${selectedCreature.hp}</div>
+        <div>${selectedCreature.ac}</div>
+        <div>${selectedCreature.fortitude}</div>
+        <div>${selectedCreature.reflex}</div>
+        <div>${selectedCreature.will}</div>
+        <div>${selectedCreature.perception}</div>
+        <div>${selectedCreature.senses}</div>
+        <div>${selectedCreature.speed}</div>
+        <div>${selectedCreature.rarity.name}</div>
+        <div class="flex flex-row">${traitsHtml}</div>
+    `;
+}
+
+// Hide the complete creature info
+function hideCreatureInfo(id) {
+    popup.classList.add('hidden');
+    content.classList.remove('hidden');
+}
+</script>
