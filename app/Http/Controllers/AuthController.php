d9d0540e72122cec63dff9baab7d1599085081a7
Michael <michael.10330@hotmail.com>
2025-06-13 14:21:56 +0200

    added email verification

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 
 class AuthController extends Controller
 {
@@ -14,18 +16,23 @@ class AuthController extends Controller
         return view('auth.login');
     }
 
-    public function login(Request $request){
+    public function login(Request $request)
+    {
         $request->validate([
             'email' => 'required|email',
             'password' => 'required',
         ]);
 
-        $user = User::where('email', $request->email);
-        if(!$user || $user->banned){
-            return back()->withErrors(['email' => 'Your account has been banned, or does not exist']);
+        $user = User::where('email', $request->email)->first();
+        if (!$user) {
+            return back()->withErrors(['email' => 'This account does not exist']);
         }
 
-        if(Auth::attempt(['email' => $request->email, 'password' => $request->password], $request->filled('remember'))){
+        if ($user->banned) {
+            return back()->withErrors(['email' => 'Your account has been banned']);
+        }
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password], $request->filled('remember'))) {
             $request->session()->regenerate();
 
             return redirect()->intended('/');
@@ -36,11 +43,13 @@ class AuthController extends Controller
         ]);
     }
 
-    public function showRegisterForm(){
+    public function showRegisterForm()
+    {
         return view('auth.register');
     }
 
-    public function register(Request $request){
+    public function register(Request $request)
+    {
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
@@ -53,12 +62,21 @@ class AuthController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
+        $adminEmail = 'admin@example.com';
+        $subject = 'New User Registration';
+        $message = "A new user has registered:\n\nName: {$user->name}\nEmail: {$user->email}";
+        Mail::raw($message, function ($mail) use ($adminEmail, $subject) {
+            $mail->to($adminEmail)
+                ->subject($subject);
+        });
+
+        event(new Registered($user));
+
         return redirect()->route('login.form')->with('message', 'You have been registered. Login to get started!');
-
-
     }
 
-    public function logout(Request $request){
+    public function logout(Request $request)
+    {
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,11 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BuilderController;
+
 use Illuminate\Auth\Middleware\AuthenticateWithBasicAuth;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
@@ -23,10 +27,33 @@ Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login.form
 Route::post('/login', [AuthController::class, 'login'])->name('login.login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('login.logout');
 
+Route::get('/email/verify', function () {
+
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+
+    $request->fulfill();
+
+
+
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+
+    $request->user()->sendEmailVerificationNotification();
+
+
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register.form');
 Route::post('/register', [AuthController::class, 'register'])->name('register.register');
 
 
 Route::get('/login/test', function () {
     return view('auth.authtest');
-})->middleware('auth');
+})->middleware(['auth', 'verified']);
