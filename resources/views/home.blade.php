9cb7614a9cd49eb5dc15f4efa6cebd464d55532b
Michael <michael.10330@hotmail.com>
2025-06-16 10:13:59 +0200

    fixed homepage tailwind temporarily

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
 @include('partials_mich.header')
 
-<h3>Placeholder homepage</h3>
+<h3 class="text-white">Placeholder homepage</h3>
 <img src="https://media.tenor.com/_AZJmhAry0gAAAAj/rat-dancing-meme.gif" alt="radtdance">
