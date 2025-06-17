6e3e98175d4b1d48c2ace427c218f720e91e5bd8
Michael <michael.10330@hotmail.com>
2025-06-17 13:27:46 +0200

    added filamentresources and some logic

---
+++ app/Filament/Resources/CreatureResource/Pages/CreateCreature.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\CreatureResource\Pages;
+
+use App\Filament\Resources\CreatureResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateCreature extends CreateRecord
+{
+    protected static string $resource = CreatureResource::class;
+}
--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -9,9 +9,11 @@ use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Filters\Filter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Hash;
 
 class UserResource extends Resource
 {
@@ -23,7 +25,15 @@ class UserResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Forms\Components\TextInput::make('name')
+                    ->required()->maxLength(255),
+                Forms\Components\TextInput::make('email')
+                    ->required()->maxLength(255)->email(),
+                Forms\Components\TextInput::make('password')
+                    ->password()
+                    ->dehydrateStateUsing(fn($state) => Hash::make($state))
+                    ->dehydrated(fn($state) => filled($state))
+                    ->required(fn(string $context): bool => $context === 'create')
             ]);
     }
 
@@ -31,10 +41,32 @@ class UserResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('name')->sortable(),
+                Tables\Columns\TextColumn::make('email')->sortable(),
+                Tables\Columns\IconColumn::make('is_admin')->sortable()
+                    ->label('Admin')
+                    ->boolean()
+                    ->trueIcon('heroicon-o-shield-check')
+                    ->falseIcon('heroicon-o-user')
+                    ->trueColor('danger')
+                    ->falseColor('gray')
+                    ->sortable(),
+                Tables\Columns\IconColumn::make('banned')->sortable()
+                    ->label('Banned')
+                    ->boolean()
+                    ->trueIcon('heroicon-o-check')
+                    ->falseIcon('heroicon-o-x-mark')
+                    ->trueColor('danger')
+                    ->falseColor('gray')
+                    ->sortable(),
             ])
             ->filters([
-                //
+                Filter::make('admin')
+                    ->query(fn(Builder $query): Builder => $query->where('is_admin', true))
+                    ->toggle(),
+                Filter::make('banned')
+                    ->query(fn(Builder $query): Builder => $query->where('banned', true))
+                    ->toggle()
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
